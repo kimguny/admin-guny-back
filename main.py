f4f35251975d28933f910642676070f93e2a55e0
kimguny <kimguny12@gmail.com>
2025-02-06 17:17:59 +0900

    ✨ cors 설정 추가

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from api.routes import login
 from core.database import engine
 from models.user import Base
 
-# Lifespan 이벤트 핸들러 설정
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    yield  # 서버가 실행될 때만 실행되고 종료 시 정리할 작업 가능
+    yield
 
 app = FastAPI(lifespan=lifespan)
 
-# 라우터 등록
-app.include_router(login.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI with PostgreSQL!"}
+app.include_router(login.router)
